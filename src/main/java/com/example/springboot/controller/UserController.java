700cdd4a6833f457599293893b0a8e40acdd2181
Kevin <kevinsmith2001233@gmail.com>
2024-03-06 16:50:22 +0800

    2024年3月6日16:50

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.springboot.bean.User;
 import com.example.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,10 +22,12 @@ public class UserController {
     }
 
     @RequestMapping("/666")
-    //@ResponseBody
-    public String e() {
-        //User user = userService.getUser();
+    protected String userList(Model model){
+        User c1 = new User();
+        c1.setId(1);
+        c1.setName("li");
+        c1.setAge(18);
+        model.addAttribute("customer",c1);
         return "customerList";
-
     }
 }
