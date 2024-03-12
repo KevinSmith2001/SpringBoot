95dbe0548691757d3ce0422580a8620d1cad64d0
Kevin <kevinsmith2001233@gmail.com>
2024-03-12 14:13:47 +0800

    2024年3月12日14.13

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -8,6 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.ObjectStreamClass;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -21,13 +26,24 @@ public class UserController {
 
     }
 
-    @RequestMapping("/666")
-    protected String userList(Model model){
+    @RequestMapping(value = "/userList")
+    protected ModelAndView userList() {
+
         User c1 = new User();
         c1.setId(1);
-        c1.setName("li");
-        c1.setAge(18);
-        model.addAttribute("customer",c1);
-        return "customerList";
+        c1.setName("张三");
+        c1.setAge(22);
+
+        //Map 提供了一个更通用的元素存储方法。Map 集合类用于存储元素对（称作“键”和“值”），其中每个键映射到一个值。
+        // 将键映射到值的对象
+        //一个映射不能包含重复的键
+        //每个键最多只能映射到一个值 :添加功能 put，删除功能 remove……
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        model.put("customer", c1);
+        //ModelAndView(String viewName, Map model) 使用Map来收集这些Model对象，然后设定给ModelAndView
+
+        ModelAndView modelAndView = new ModelAndView("customerList", model);
+        return modelAndView;
     }
 }
